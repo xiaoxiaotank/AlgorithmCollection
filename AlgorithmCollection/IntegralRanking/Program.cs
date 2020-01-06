646eb8bfa51a07d7e9bcd99611b7c1927355b0b3
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2020-01-06 13:40:00 +0800

    增加积分排名注释

--- AlgorithmCollection/IntegralRanking/Program.cs
+++ AlgorithmCollection/IntegralRanking/Program.cs
@@ -8,6 +8,11 @@ using System.Linq;
 /// 积分排名
 /// 前提：积分为非负整数
 /// 参考：https://www.cnblogs.com/hohoa/p/12145689.html 、https://www.cnblogs.com/weidagang2046/archive/2012/03/01/massive-user-ranking.html#!comments
+/// 
+/// 原理：所构建的树节点均为左节点小于右节点，
+///     再将用户积分插入到树之后，每个右节点则存储经过该节点的数量Count
+///     当查询Integral排名时，若Integral落在左节点范围内，则之前经过的右节点的Sum(Count)则为大于该Integral的积分数量，也就是排名
+///     由于积分数量从0开始，排名从1开始，所以最后要+1
 /// </summary>
 namespace IntegralRanking
 {
